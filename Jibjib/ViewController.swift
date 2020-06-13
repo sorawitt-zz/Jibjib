5d9ca53f8d9bb3a61209ca23513ca497742ff74d
Sorawit Trutsat <sorawit@central.tech>
2020-06-13 15:09:40 +0700

    Initial Commit

---
+++ Jibjib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jibjib
+//
+//  Created by sorawit on 13/6/2563 BE.
+//  Copyright © 2563 sorawit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
